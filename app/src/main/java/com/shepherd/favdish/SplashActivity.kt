579d53d9a2670882d613ac1410b833985e59ca37
Shepherd Siduli <shepherdsiduli@gmail.com>
2021-09-21 00:26:10 +0200

    Initial commit

---
+++ app/src/main/java/com/shepherd/favdish/SplashActivity.kt
@@ -0,0 +1,27 @@
+package com.shepherd.favdish
+
+import android.os.Build
+import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.shepherd.favdish.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                    WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+    }
+}
